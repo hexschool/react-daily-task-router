38222a0396874e076efc7451587bef32307aa67a
chenminfan <mingfan1202@gmail.com>
2022-08-23 01:03:03 +0800

    feat: add id page
    feat: add id page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import {
+  Routes,
+  Route,
+  useNavigate,
+  useParams,
   HashRouter,
   NavLink,
+  Link
 } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-const Todo = () => {
+const Logout = () =>{
+  const navigate = useNavigate();
+  return <button className="nav-link" onClick={()=>{navigate('/login',{ replace: true })}}>Logout</button>
+}
+const Home = () => {
+  return <p>哇係首頁</p>
+  }
+const Todo = () => {    
   return <p>這是 Todo 頁面 
   </p>;
 };
@@ -13,6 +26,28 @@ const Login = () => {
 };
 const Register = () => {
   return <p>這是註冊頁面</p>;
+};
+const Id = () => {
+  const{Id} = useParams();
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    fetch("https://api.kcg.gov.tw/api/service/Get/9c8e1450-e833-499c-8320-29b36b7ace5c")
+    .then(res=>res.json())
+    .then(result=>{
+      const newData = result.data.XML_Head.Infos.Info
+      setData(newData);
+    })
+  },[])
+  return (
+    <div className="idPage">
+      <p>這是註冊頁面</p>
+      {data.map((item,index) =>{
+        return(
+          <div className="item" key={index}><Link to={`../id/${item.Id}`}>{item.Name}</Link></div>
+        )
+      })}
+    </div>
+  );
 };
 
 function App() {
@@ -32,8 +67,19 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/id">
+            <p>Id 頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route index element={<Home />} /> 
+          <Route path="/register" element={<Register />} /> 
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/todo" element={<Todo />} /> 
+          <Route path=":Id" element={<Id />} /> 
+        </Routes>
+        
         {/* 練習區 */}
       </HashRouter>
     </div>
